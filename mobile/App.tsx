375af816874a4ad410bd4643a3b6dfcb99c5930c
ErickSilva2605 <erick.dev.it@gmail.com>
2022-09-12 22:54:55 -0300

    Adicionando projeto mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Bora fi do billll, boraaa mulher do biillll!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
