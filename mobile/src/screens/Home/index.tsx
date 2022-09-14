a6f5dbbb7d07b55bf87f8b820b56ab4c09d3d8ef
ErickSilva2605 <erick.dev.it@gmail.com>
2022-09-14 03:42:36 -0300

    Criando o componente de Card e a lista de games

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,8 +1,10 @@
-import { View, Image } from 'react-native';
+import { View, Image, FlatList } from 'react-native';
 
 import logoImage from '../../assets/logo-nlw-esports.png';
+import { GameCard } from '../../components/GameCard';
 import { Heading } from '../../components/Heading';
 
+import { GAMES } from '../../utils/games';
 import { styles } from './styles';
 
 export function Home() {
@@ -14,6 +16,19 @@ export function Home() {
       />
 
       <Heading title="Encontre seu duo!" subtitle="Selecione o game que deseja jogar..." />
+
+      <FlatList
+        data={GAMES}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) =>
+          <GameCard
+            data={item}
+          />
+        }
+        showsHorizontalScrollIndicator={false}
+        horizontal
+        contentContainerStyle={styles.contentList}
+      />
     </View>
   );
 }
