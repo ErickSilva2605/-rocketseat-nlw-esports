94ade11dfb5b20c62b227722f7ccad0acc4def6a
ErickSilva2605 <erick.dev.it@gmail.com>
2022-09-14 02:43:41 -0300

    Criando o componente de cabeçalho

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,11 +1,19 @@
-import { View } from 'react-native';
+import { View, Image } from 'react-native';
+
+import logoImage from '../../assets/logo-nlw-esports.png';
+import { Heading } from '../../components/Heading';
 
 import { styles } from './styles';
 
 export function Home() {
   return (
     <View style={styles.container}>
+      <Image
+        source={logoImage}
+        style={styles.logo}
+      />
 
+      <Heading title="Encontre seu duo!" subtitle="Selecione o game que deseja jogar..." />
     </View>
   );
 }
