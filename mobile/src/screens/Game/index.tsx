6b310a5d06b6f05aefa80224008d6df0c88ffff5
ErickSilva2605 <erick.dev.it@gmail.com>
2022-09-16 04:32:08 -0300

    Implementando header e cover da tela de game

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -1,18 +1,57 @@
+import { View, TouchableOpacity, Image } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { useRoute } from '@react-navigation/native';
+import { useRoute, useNavigation } from '@react-navigation/native';
+import { Entypo } from '@expo/vector-icons';
 
-import { Background } from '../../components/Background';
+import logoImage from '../../assets/logo-nlw-esports.png';
 
 import { styles } from './styles';
+import { THEME } from '../../theme';
+
 import { GameParams } from '../../@types/navigation';
 
+import { Background } from '../../components/Background';
+import { Heading } from '../../components/Heading';
+
 export function Game() {
+  const navigation = useNavigation();
   const route = useRoute();
   const game = route.params as GameParams;
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
 
   return (
     <Background>
       <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity onPress={handleGoBack}>
+            <Entypo
+              name="chevron-thin-left"
+              color={THEME.COLORS.CAPTION_300}
+              size={20}
+            />
+          </TouchableOpacity>
+          <Image
+            source={logoImage}
+            style={styles.logo}
+          />
+          <View style={styles.right} />
+        </View>
+
+        <View style={styles.coverContainer}>
+          <Image
+            source={{ uri: game.bannerUrl }}
+            style={styles.cover}
+            resizeMode="cover"
+          />
+        </View>
+
+        <Heading
+          title={game.title}
+          subtitle="Conecte-se e comece a jogar!"
+        />
 
       </SafeAreaView>
     </Background>
--- mobile/src/screens/Game/styles.ts
+++ mobile/src/screens/Game/styles.ts
@@ -1,5 +1,34 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-  container: {}
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  header: {
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 32,
+    marginTop: 28
+  },
+  logo: {
+    width: 72,
+    height: 40
+  },
+  right: {
+    width: 20,
+    height: 20
+  },
+  coverContainer: {
+    width: '100%',
+    paddingHorizontal: 32,
+    marginTop: 32
+  },
+  cover: {
+    width: '100%',
+    height: 180,
+    borderRadius: 8
+  }
 });
