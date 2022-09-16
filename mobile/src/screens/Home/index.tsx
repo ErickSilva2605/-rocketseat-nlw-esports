1eabe652cc5ee30df6b92177d161ee456ba5a70d
ErickSilva2605 <erick.dev.it@gmail.com>
2022-09-16 02:02:45 -0300

    Obtendo a lista de games da api no mobile

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,13 +1,21 @@
+import { useState, useEffect } from 'react';
 import { View, Image, FlatList } from 'react-native';
 
 import logoImage from '../../assets/logo-nlw-esports.png';
-import { GameCard } from '../../components/GameCard';
+import { GameCard, GameCardProps } from '../../components/GameCard';
 import { Heading } from '../../components/Heading';
 
-import { GAMES } from '../../utils/games';
 import { styles } from './styles';
 
 export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([]);
+
+  useEffect(() => {
+    fetch('http://192.168.15.12:3333/games')
+      .then(response => response.json())
+      .then(data => setGames(data))
+  }, []);
+
   return (
     <View style={styles.container}>
       <Image
@@ -18,7 +26,7 @@ export function Home() {
       <Heading title="Encontre seu duo!" subtitle="Selecione o game que deseja jogar..." />
 
       <FlatList
-        data={GAMES}
+        data={games}
         keyExtractor={item => item.id}
         renderItem={({ item }) =>
           <GameCard
